b2a0a0d97a915cd8ff58cfaa004b030610b37f53
Paul La Plante <plaplant@sas.upenn.edu>
2021-11-02 22:52:10 +0000

    Add explicit absolute path for scripts

--- src/scripts/hera_xeng_start.sh
+++ src/scripts/hera_xeng_start.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 source ~/.bashrc
-set -e # exit with an error if any subcommand returns an error; .bashrc has such a command!
+#set -e # exit with an error if any subcommand returns an error; .bashrc has such a command!
 
 LOGFILE=~/xeng_start.log
 export TAG=engineering
 export CATCHERHOST=hera-sn1
 
 # Kill off anything still running
-hera_xeng_stop.sh
+/usr/local/bin/hera_xeng_stop.sh
 
 # Start afresh
-echo "Starting X-Engines" > $LOGFILE
-date > $LOGFILE
-xtor_up.py --runtweak --redislog px{1..16} &> $LOGFILE
-hera_catcher_up.py --redislog $CATCHERHOST &> $LOGFILE
+echo Starting X-Engines > $LOGFILE
+date >> $LOGFILE
+/usr/local/bin/xtor_up.py --runtweak --redislog px{1..16} &>> $LOGFILE
+/usr/local/bin/hera_catcher_up.py --redislog $CATCHERHOST &>> $LOGFILE
 
 # XXX add arguments to set_observation
-hera_set_observation.py &> $LOGFILE
-hera_ctl.py start &> $LOGFILE
-hera_catcher_take_data.py --tag $TAG $CATCHERHOST &> $LOGFILE
+/usr/local/bin/hera_set_observation.py &>> $LOGFILE
+/usr/local/bin/hera_ctl.py start &>> $LOGFILE
+/usr/local/bin/hera_catcher_take_data.py --tag $TAG $CATCHERHOST &>> $LOGFILE
