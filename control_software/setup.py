01593cc63a31da68d7440721b82e1ade55c94ed7
Paul La Plante <plaplant@sas.upenn.edu>
2022-04-27 13:42:31 -0700

    Use setuptools_scm to handle version info

--- control_software/setup.py
+++ control_software/setup.py
@@ -1,31 +1,25 @@
-from distutils.core import setup
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2022 The HERA Collaboration
+# Licensed under the 2-Clause BSD License
+
+from setuptools import setup
+from setuptools_scm import get_version
 import glob
 import os
 
-ver = '0.0.1'
-try:
-    import subprocess
-    ver = ver + '-' + subprocess.check_output(['git', 'describe', '--abbrev=8', '--always', '--dirty', '--tags']).strip()
-except:
-    print('Couldn\'t get version from git. Defaulting to %s' % ver)
+setup(
+    name='paper_gpu',
+    description='Python libraries and scripts to control the HERA correlator X-Engines',
+    author='Aaron Parsons',
+    author_email='aparsons@berkeley.edu',
+    url='https://github.com/HERA-Team/paper_gpu',
+    provides=['paper_gpu'],
+    packages=['paper_gpu'],
+    package_dir={'paper_gpu': 'src'},
+    scripts=glob.glob('scripts/*.py') + glob.glob('scripts/*.sh'),
+)
 
-# Generate a __version__.py file with this version in it
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'src', '__version__.py'), 'w') as fh:
-    fh.write('__version__ = "%s"' % ver)
-
-setup(name='paper_gpu',
-      version='%s' % ver,
-      description='Python libraries and scripts to control the HERA correlator X-Engines',
-      author='Aaron Parsons',
-      author_email='aparsons@berkeley.edu',
-      url='https://github.com/HERA-Team/paper_gpu',
-      provides=['paper_gpu'],
-      packages=['paper_gpu'],
-      package_dir={'paper_gpu': 'src'},
-      scripts=glob.glob('scripts/*.py')+glob.glob('scripts/*.sh'),
-      )
-
+ver = get_version(root="..", local_scheme="dirty-tag")
 if ver.endswith("dirty"):
     print ("********************************************") 
     print ("* You are installing from a dirty git repo *")
