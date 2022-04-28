72a2e8e48e7f7013178d8b1be49b14bfcc38a6b7
Paul La Plante <plaplant@sas.upenn.edu>
2022-04-27 19:00:17 -0700

    Reformat setup.py slightly

--- control_software/setup.py
+++ control_software/setup.py
@@ -11,25 +11,25 @@ with io.open("README.md", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='paper_gpu',
-    description='Python libraries and scripts to control the HERA correlator X-Engines',
+    name="paper_gpu",
+    description="Python libraries and scripts to control the HERA correlator X-Engines",
     long_description=readme,
     long_description_content_type="text/markdown",
     license="BSD",
-    author='Aaron Parsons',
-    author_email='aparsons@berkeley.edu',
-    url='https://github.com/HERA-Team/paper_gpu',
+    author="Aaron Parsons",
+    author_email="aparsons@berkeley.edu",
+    url="https://github.com/HERA-Team/paper_gpu",
     packages=find_namespace_packages("src"),
     package_dir={"": "src"},
-    scripts=glob.glob('scripts/*.py') + glob.glob('scripts/*.sh'),
+    scripts=glob.glob("scripts/*.py") + glob.glob("scripts/*.sh"),
     include_package_data=True,
 )
 
 ver = get_version(root="..", local_scheme="dirty-tag")
 if ver.endswith("dirty"):
-    print ("********************************************") 
-    print ("* You are installing from a dirty git repo *")
-    print ("*      One day you will regret this.       *")
-    print ("*                                          *")
-    print ("*  Consider cleaning up and reinstalling.  *")
-    print ("********************************************")
+    print("********************************************")
+    print("* You are installing from a dirty git repo *")
+    print("*      One day you will regret this.       *")
+    print("*                                          *")
+    print("*  Consider cleaning up and reinstalling.  *")
+    print("********************************************")
