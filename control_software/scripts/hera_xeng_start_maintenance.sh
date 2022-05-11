38a71a62b63f458f6de63089bb5bc73c6b651767
Aaron Parsons <aparsons@berkeley.edu>
2022-05-11 16:38:38 -0700

    Added command-line parameters to hera_xeng_start_maintenance.sh

--- control_software/scripts/hera_xeng_start_maintenance.sh
+++ control_software/scripts/hera_xeng_start_maintenance.sh
@@ -4,8 +4,38 @@
 source ~/hera-venv/bin/activate hera
 
 LOGFILE=~/xeng_start.log
-export TAG=engineering
-export CATCHERHOST=hera-sn1
+CATCHERHOST=hera-sn1
+
+Help()
+{
+   echo "Options:"
+   echo "o     Observation length in hours. Default 2"
+   echo "t     Observing tag. Default 'engineering'"
+   echo "h     This help"
+   echo
+}
+
+# Default values
+obslen="2"
+TAG=engineering
+
+# Process input arguments
+while getopts "ho:t:" option; do
+    case $option in
+      h) # display Help
+         Help
+         exit;;
+      o) # obslen
+         obslen="$OPTARG"
+         ;;
+      t) # tag
+         TAG="$OPTARG"
+         ;;
+     \?) # Invalid option
+         Help
+         exit;;
+    esac
+done
 
 # Kill off anything still running
 hera_xeng_stop.sh
@@ -16,7 +46,6 @@ date >> $LOGFILE
 xtor_up.py --runtweak --redislog px{1..16} &>> $LOGFILE
 hera_catcher_up.py --redislog $CATCHERHOST &>> $LOGFILE
 
-# XXX add arguments to set_observation
-hera_set_observation.py --obslen=2 &>> $LOGFILE
+hera_set_observation.py --obslen=$obslen &>> $LOGFILE
 hera_ctl.py start &>> $LOGFILE
 hera_catcher_take_data.py --tag $TAG $CATCHERHOST &>> $LOGFILE
