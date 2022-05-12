57abcdfbc4b7f58ecc71ad2ef3cede109fbc2703
HERA User <hera@hera.pvt>
2022-05-12 00:47:22 +0000

    Made hera_xeng scripts executable and added some echo logging to hera_xeng_start_maintenance to reflect argument passing

--- control_software/scripts/hera_xeng_start_maintenance.sh
+++ control_software/scripts/hera_xeng_start_maintenance.sh
@@ -43,9 +43,14 @@ hera_xeng_stop.sh
 # Start afresh
 echo Starting X-Engines > $LOGFILE
 date >> $LOGFILE
+echo xtor_up.py --runtweak --redislog px{1..16} &>> $LOGFILE
 xtor_up.py --runtweak --redislog px{1..16} &>> $LOGFILE
+echo hera_catcher_up.py --redislog $CATCHERHOST &>> $LOGFILE
 hera_catcher_up.py --redislog $CATCHERHOST &>> $LOGFILE
 
+echo hera_set_observation.py --obslen=$obslen &>> $LOGFILE
 hera_set_observation.py --obslen=$obslen &>> $LOGFILE
+echo hera_ctl.py start &>> $LOGFILE
 hera_ctl.py start &>> $LOGFILE
+echo hera_catcher_take_data.py --tag $TAG $CATCHERHOST &>> $LOGFILE
 hera_catcher_take_data.py --tag $TAG $CATCHERHOST &>> $LOGFILE
