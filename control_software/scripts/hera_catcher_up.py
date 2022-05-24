a3552af2e4be0ce63e9f03e2008f604ee02b585d
Paul La Plante <plaplant@sas.upenn.edu>
2022-05-23 17:01:54 -0700

    Remove block of commented out code

--- control_software/scripts/hera_catcher_up.py
+++ control_software/scripts/hera_catcher_up.py
@@ -67,17 +67,6 @@ time.sleep(15)
 cpu_mask = '0x0004'
 run_on_hosts([args.host], ['taskset', cpu_mask, 'hashpipe_redis_gateway.rb', '-g', args.host, '-i', '0'])
 
-# Wait for the gateways to come up
-# time.sleep(15)
-
-# Upload config file location
-# NOTE: This has to come before template generation!
-# Generate the BDA config file and upload to redis
-# THIS SHOULD BE DONE ALREADY DURING xtor_up.py
-# if not args.nobda:
-#     print('Create configuration file')
-#     run_on_hosts([args.host], python_source_cmd + ['hera_create_bda_config.py','-c','-r'], wait=True)
-
 time.sleep(10)
 
 # Generate the meta-data template
