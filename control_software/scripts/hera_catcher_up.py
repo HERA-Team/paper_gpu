4455b6b32c2e2ef456f1d49f26585d7f90061d1e
HERA User <hera@hera.pvt>
2022-09-20 20:19:01 +0000

    Reduced catcher ring buffer back to 4, added python package install to install_hera, and reduced sleep time in hera_catcher_up

--- control_software/scripts/hera_catcher_up.py
+++ control_software/scripts/hera_catcher_up.py
@@ -9,7 +9,6 @@ import os
 from paper_gpu import bda
 
 perf_tweaker = 'tweak-perf-sn.sh'
-init = 'hera_catcher_init.sh'
 
 def run_on_hosts(hosts, cmd, user=None, wait=True):
     if isinstance(cmd, str):
@@ -57,15 +56,15 @@ if args.redislog:
    init_args += ['-r']
 
 # Start Catcher
-run_on_hosts([args.host], python_source_cmd + ['cd', '/data;', init] + init_args + ['0'], wait=True)
-time.sleep(15)
+run_on_hosts([args.host], python_source_cmd + ['cd', '/data;', 'hera_catcher_init.sh'] + init_args + ['0'], wait=True)
+time.sleep(5)
 
 # Start hashpipe<->redis gateways
 cpu_mask = '0x0004'
 run_on_hosts([args.host], ['taskset', cpu_mask, 'hashpipe_redis_gateway.rb', '-g', args.host, '-i', '0'])
 
 # Wait for the gateways to come up
-time.sleep(10)
+time.sleep(5)
 
 # Generate the meta-data template
 run_on_hosts([args.host], python_source_cmd + ["hera_init_catcher_data.py"] + ["--verbose"], wait=True)
