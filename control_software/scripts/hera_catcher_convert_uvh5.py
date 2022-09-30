653854d6ff7aa1231bd63506b90efcc3cbf2665d
HERA User <hera@hera.pvt>
2022-09-30 20:04:39 +0000

    Created a working daemon script hera_catcher_convert.uvh5 that will draw down a redis buffer of raw files and convert them to finished files.

--- control_software/scripts/hera_catcher_convert_uvh5.py
+++ control_software/scripts/hera_catcher_convert_uvh5.py
@@ -5,14 +5,16 @@ import re
 import os
 import logging
 import psutil
+import signal
+from paper_gpu.file_conversion import make_uvh5_file
 
 logger = logging.getLogger(__file__)
 
 REDISHOST = 'redishost'
 DATA_DIR = '/data'
-RAW_EXT = 'dat'
 TEMPLATE = re.compile(r'zen\.(\d+)\.(\d+)\.(sum|diff)\.dat')
 RAW_FILE_KEY = 'corr:raw_files'
+PURG_FILE_KEY = 'corr:purgatory_files'
 CONV_FILE_KEY = 'corr:converted_files'
 CUR_FILE_KEY = 'corr:current_file'
 CPU_AFFINITY = list(range(6))  # the rest are reserved for the catcher
@@ -24,51 +26,78 @@ def match_up_filenames(f, cwd=None):
     jd_day, jd_frac, sum_diff = TEMPLATE.match(f_in).groups()
     f_in = os.path.join(path, f_in)
     f_meta = os.path.join(path, f'zen.{jd_day}.{jd_frac}.meta.hdf5')
-    f_out = os.path.join(path, f'zen.{jd_day}.{jd_frac}.{sum_diff}.hdf5')
+    f_out = os.path.join(path, f'zen.{jd_day}.{jd_frac}.{sum_diff}.uvh5')
     return (f_in, f_meta, f_out)
 
-def get_cwd_from_redis(r):
-    return r.hget('corr', 'catcher_cwd')
+def get_cwd_from_redis(r, default='/data'):
+    cwd = r.hget('corr', 'catcher_cwd')
+    if cwd is None:
+        return default
+    else:
+        return cwd
 
-def process_next(r, cwd):
+def process_next(f, cwd):
     p = psutil.Process()
     p.cpu_affinity(CPU_AFFINITY)
-    if r.llen(RAW_FILE_KEY) == 0:
-        return
-    # once we get a key, we either have to finish it or return it; no dropping
-    logger.info(f'Processing {f}')
-    f = r.rpop(RAW_FILE_KEY)  # process most recent first (LIFO)
-    try:
-        f_in, f_meta, f_out = match_up_filenames(f, cwd)
-        make_uvh5_file(f_out, f_meta, f_in)
-        f_out_rel = os.path.relpath(f_out, cwd)
-        r.rpush(CONV_FILE_KEY, f_out_rel)  # document we finished it
-        logger.info(f'Finished {f}')
-    except Exception as e:
-        r.lpush(RAW_FILE_KEY, f)  # otherwise return key at back of queue
-        logger.error(f'Failed to finish {f}. Returned to queue.\n' + str(e))
+    print(f'Processing {f}')
+    f_in, f_meta, f_out = match_up_filenames(f, cwd)
+    make_uvh5_file(f_out, f_meta, f_in)
+    f_out_rel = os.path.relpath(f_out, cwd)
+    r.rpush(CONV_FILE_KEY, f_out_rel)  # document we finished it
+    r.hdel(PURG_FILE_KEY, f)
+    print(f'Finished {f_in} -> {f_out}')
     
 if __name__ == '__main__':
     import multiprocessing as mp
     import redis
     import time
+    import sys
     import argparse
+    logging.basicConfig(level=logging.DEBUG)
+
+    p = psutil.Process()
+    p.cpu_affinity(CPU_AFFINITY)
 
     assert psutil.cpu_count() == 12, "if this errors, you're not on hera-sn1"
 
-    r = redis.Redis(REDISHOST)
+    r = redis.Redis(REDISHOST, decode_responses=True)
     cwd = get_cwd_from_redis(r)
-    with mp.Pool(len(CPU_AFFINITY)) as pool:
-        nworkers = pool._processes
+    qlen = r.llen(RAW_FILE_KEY)
+    print(f'Starting conversion. {cwd}')
+    print(f'Starting conversion. Queue length={qlen}. N workers={len(CPU_AFFINITY)}')
+    children = {}
+    nworkers = len(CPU_AFFINITY)
+    try:
         while True:
-            try:
-                qlen = r.llen(RAW_FILE_KEY)
-                active = len(mp.active_children())
-                logger.info(f'Queue length={qlen}, N workers={active}/{nworkers}')
-                if qlen > 0 and len(mp.active_children()) < nworkers:
-                    pool.apply_async(process_next, (r, cwd))
-                else:
-                    time.sleep(1)
-            except(KeyboardInterrupt):
-                pool.close()
-                pool.join()
+            qlen = r.llen(RAW_FILE_KEY)
+            children = {f: thd for f, thd in children.items() if thd.is_alive()}
+            print(f'Queue length={qlen}, N workers={len(children)}/{nworkers}')
+            if qlen > 0 and len(children) < nworkers:
+                # once we get a key, we commit to finish it or return it; no dropping
+                f = r.rpop(RAW_FILE_KEY)  # process most recent first (LIFO)
+                r.hset(PURG_FILE_KEY, f, 0)
+                print(f'Starting worker on {f}')
+                thd = mp.Process(target=process_next, args=(f, cwd))
+                thd.start()
+                children[f] = thd
+            else:
+                time.sleep(1)
+    except Exception as e:
+        print(f'Closing down {len(children)} threads')
+        for f, thd in children.items():
+            thd.terminate()
+        for thd in children.values():
+            thd.join()
+    finally:
+        print('Cleanup')
+        purgfiles = r.hgetall(PURG_FILE_KEY)
+        for f in purgfiles:
+            print(f'Returning {f}')
+            r.rpush(RAW_FILE_KEY, f)
+            r.hdel(PURG_FILE_KEY, f)
+            f_in, f_meta, f_out = match_up_filenames(f, cwd)
+            print(f'Remove {f_out}?')
+            if os.path.exists(f_out):
+                print(f'Removing {f_out}')
+                os.remove(f_out)
+        sys.exit(0)
