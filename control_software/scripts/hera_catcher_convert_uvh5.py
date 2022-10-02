6b757caf79fba7c329f389af89efce876cde205d
HERA User <hera@hera.pvt>
2022-10-02 22:25:40 +0000

    Reverted to no-mc file conversion. Will move m/c into separate daemon

--- control_software/scripts/hera_catcher_convert_uvh5.py
+++ control_software/scripts/hera_catcher_convert_uvh5.py
@@ -5,10 +5,7 @@ import re
 import os
 import psutil
 import signal
-import numpy as np
 from paper_gpu.file_conversion import make_uvh5_file
-from astropy.time import Time  # XXX remove this dependency too?
-from hera_mc import mc
 
 DELETE_TAGS = ('delete', 'junk')
 REDISHOST = 'redishost'
@@ -17,7 +14,6 @@ TEMPLATE = re.compile(r'zen\.(\d+)\.(\d+)\.(sum|diff)\.dat')
 RAW_FILE_KEY = 'corr:raw_files'
 PURG_FILE_KEY = 'corr:purgatory_files'
 CONV_FILE_KEY = 'corr:converted_files'
-CUR_FILE_KEY = 'corr:current_file'
 CPU_AFFINITY = list(range(6))  # the rest are reserved for the catcher
 
 def match_up_filenames(f, cwd=None):
@@ -37,7 +33,7 @@ def get_cwd_from_redis(r, default='/data'):
     else:
         return cwd
 
-def process_next(f, cwd, db=None):
+def process_next(f, cwd):
     p = psutil.Process()
     p.cpu_affinity(CPU_AFFINITY)
     print(f'Processing {f}')
@@ -47,45 +43,8 @@ def process_next(f, cwd, db=None):
     r.rpush(CONV_FILE_KEY, f_out_rel)  # document we finished it
     r.hdel(PURG_FILE_KEY, f)
     print(f'Finished {f_in} -> {f_out}')
-    if db is not None:
-        add_to_mc_and_rtp(db, f_out, info)
 
-def add_to_mc_and_rtp(db, f, info):
-    tag = info['tag']
-    if tag in DELETE_TAGS:
-        print(f'Skipping tag {tag} in {f}')
-        return
-    times = np.unique(info['time_array'])
-    starttime = Time(times[0], scale='utc', format='jd')
-    stoptime = Time(times[-1], scale='utc', format='jd')
-    obsid = int(np.floor(starttime.gps))
-    int_jd = int(np.floor(starttime.jd))
-    filename = os.path.basename(uvfile)
-    hostname = socket.gethostname()
-    if "hera-sn1" in hostname:
-        prefix = os.path.join("/mnt/sn1", f"{int_jd:d}")
-    elif "hera-sn2" in hostname:
-        prefix = os.path.join("/mnt/sn2", f"{int_jd:d}")
-    else:
-        # default
-        prefix = "unknown"
 
-    with db.sessionmaker() as session:
-        obs = session.get_obs(obsid)
-        if len(obs) > 0:
-            print(f'observation {obsid} for file {f} already in M&C, skipping')
-        else:
-            print(f'Inserting {obsid} for file {f} in M&C')
-            session.add_obs(starttime, stoptime, obsid, tag)
-        print(f'Inserting {obsid} for file {f} in RTP')
-        result = session.get_rtp_launch_record(obsid)
-        if len(result) == 0:
-            session.add_rtp_launch_record(obsid, int_jd, obs_tag, filename, prefix)
-        else:
-            t0 = Time.now()
-            session.update_rtp_launch_record(obsid, t0)
-        session.commit()
-    
 if __name__ == '__main__':
     import multiprocessing as mp
     import redis
@@ -98,7 +57,6 @@ if __name__ == '__main__':
     assert psutil.cpu_count() == 12, "if this errors, you're not on hera-sn1"
 
     r = redis.Redis(REDISHOST, decode_responses=True)
-    db = mc.connect_to_mc_db(None)
     cwd = get_cwd_from_redis(r)
     qlen = r.llen(RAW_FILE_KEY)
     print(f'Starting conversion. {cwd}')
@@ -115,7 +73,7 @@ if __name__ == '__main__':
                 f = r.rpop(RAW_FILE_KEY)  # process most recent first (LIFO)
                 r.hset(PURG_FILE_KEY, f, 0)
                 print(f'Starting worker on {f}')
-                thd = mp.Process(target=process_next, args=(f, cwd, db))
+                thd = mp.Process(target=process_next, args=(f, cwd))
                 thd.start()
                 children[f] = thd
             else:
