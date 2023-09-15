52099a23e8b667cfc79dbe43d864e633dd0d26d5
David MacMahon <davidm@berkeley.edu>
2023-09-15 16:01:04 +0000

    Use IB verbs for the X engine packet reception

--- control_software/scripts/hera_xeng_start.sh
+++ control_software/scripts/hera_xeng_start.sh
@@ -13,7 +13,7 @@ hera_xeng_stop.sh
 # Start afresh
 echo Starting X-Engines > $LOGFILE
 date >> $LOGFILE
-xtor_up.py --runtweak --redislog --noibverbs px{1..16} &>> $LOGFILE
+xtor_up.py --runtweak --redislog px{1..16} &>> $LOGFILE
 hera_catcher_up.py --redislog &>> $LOGFILE
 
 # XXX add arguments to set_observation
